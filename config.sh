863092100646c48b977f474635db8e096832db37
khuuvandoan <104571793+khuuvandoan@users.noreply.github.com>
2022-08-08 23:34:38 +0700

    Delete config.sh

--- config.sh
+++
@@ -1,6 +0,0 @@
-cd /etc/XrayR/config
-rm config.yml custom_inbound.json custom_outbound.json dns.json geoip.dat geosite.dat route.json rulelist zingfast.crt zingfast.key 
-cd /etc/XrayR/config
-wget https://zingfast.vn/config.zip 
-unzip config.zip 
-cd /etc/XrayR/config
