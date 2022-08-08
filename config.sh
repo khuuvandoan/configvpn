8b6562d38797d50d1c34f3c8c0a81016ff398fea
khuuvandoan <104571793+khuuvandoan@users.noreply.github.com>
2022-08-08 23:20:16 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 cd /etc/XrayR 
 rm config.yml custom_inbound.json custom_outbound.json dns.json geoip.dat geosite.dat route.json rulelist zingfast.crt zingfast.key 
-
+cd /etc/XrayR 
 wget https://zingfast.vn/config.zip 
 unzip config.zip 
+cd /etc/XrayR 
